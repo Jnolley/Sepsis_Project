4bce8aef27218881e1cda38061591d5461ce56e4
jackmnolley <jackmnolley@gmail.com>
2020-10-30 11:42:35 -0500

    Initial Commit

---
+++ sepsis/src/main/java/edu/unl/cse/soft160/a4/sepsis/App.java
@@ -0,0 +1,13 @@
+package edu.unl.cse.soft160.a4.sepsis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
