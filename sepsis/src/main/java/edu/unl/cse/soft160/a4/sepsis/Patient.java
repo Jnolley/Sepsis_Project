4b30c21008e444d655fa6ba211dfd1a684bd700e
jackmnolley <jackmnolley@gmail.com>
2020-11-13 11:02:16 -0600

    Update observations in patient class

--- sepsis/src/main/java/edu/unl/cse/soft160/a4/sepsis/Patient.java
+++ sepsis/src/main/java/edu/unl/cse/soft160/a4/sepsis/Patient.java
@@ -47,11 +47,32 @@ public class Patient {
 	public void setBirthDate(LocalDate birthDate) {
 		this.birthDate = birthDate;
 	}
+
 	public void addAllValueToObservationList() {
 		Observation isPregnant = new Observation("isPregnant");
 		Observation temperature = new Observation("temperature");
 		Observation heartRate =  new Observation("heartRate");
-		Observation respiratoryRate =  new Observation("respiratoryRate");
+		Observation respiratoryRate =  new Observation("respiratoryRate"); // this is a calculated value we need to change
+		Observation wBCCount= new Observation("wBCCount");
+		Observation systolicBloodPressure= new Observation ("systolicBloodPressure");
+		Observation diastolicBloodPressure= new Observation ("diastolicBloodPressure");
+		Observation isInpatientStatus= new Observation ("isInpatientStatus");
+		Observation needForVasopressorSupport= new Observation ("needForVasopressorSupport");
+		Observation isResponsive= new Observation ("isResponsive");
+		Observation cardiacArrhythmia= new Observation ("cardiacArrhythmia");
+		Observation declineInBaselineMentalStatus= new Observation ('declineInBaselineMentalStatus'); 
+		Observation infectionSkinWound= new Observaiton ("infectionSkinWound");
+		Observation infectionInvasiveDevice= new Observation("infectionInvasiveDevice");
+		Observation infectionRecentSurgicalProcedure= new Observation("infectionRecentSurgicalProcedure");
+		Observation isImmunocompromised= new Observation ("isImmunocompromised");
+		Observation respiratoryPaOxyFiOxy= new Observation ("respiratoryPaOxyFiOxy"); //check this one this one might also be calculated
+		Observation coagulationPlatelets= new Observation ("coagualtionPlatelets"); 
+		Observation liverBilirubin= new Observation ("liverBilirubin");
+		Observation dopamine= new Observation ("dopamine");
+		Observation anyDoputamine= new Observation ("anyDoputamine");
+		Observation epinephrine= new Observation ("epinephrine");
+		Observation norepinephrine= new Observation ("norepinephrine");
+		Observation glasgowComaScale= new Observation ("galsgowComaScale");
 	}
 
 	public ArrayList<Observation> getObservationList() {
